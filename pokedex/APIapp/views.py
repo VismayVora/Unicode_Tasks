61025e8fbb5ba0fc0931048cf1c0fa8a00c4e5ec
Vismay Vora <voravismay9@gmail.com>
2021-08-05 00:28:58 +0530

    Minor Improvements - Task 2

--- pokedex/APIapp/views.py
+++ pokedex/APIapp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests
 from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+
+#def index(request):
+     #return HttpResponse("<h1>Trying to implement Pokedex Task</h1>")
 
 def index(request):
-     return HttpResponse("<h1>Trying to implement Pokedex Task</h1>")
+     r = requests.get('https://pokeapi.co/api/v2/type/')
+     if r.status_code == 200:
+          return HttpResponse('Yay! It worked!\n{}'.format(r.text))
+
+
+#def index(request):
+#     r = requests.get('https://pokeapi.co/api/v2/type/')
+#     if r.status_code == 200:
+#        return JsonResponse(r.text, safe=False)
