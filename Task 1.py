a99fe1b84c58cac946360cce9d612b47293dd72b
Vismay Vora <voravismay9@gmail.com>
2021-08-05 20:28:41 +0530

    Part of Bonus Task

--- Task 1.py
+++ Task 1.py
@@ -1,4 +1,4 @@
-n = int(input("Enter the number of words\n"))
+n = int(input("\nEnter the number of words\n"))
 sequence = ""
 
 for i in range(n):
@@ -15,11 +15,31 @@ for word in sequence_list:
     else:
         words_dict[word] = 1
 
-words_dict.pop("")
-print(words_dict)
+words_dict.pop("") 
+#print(words_dict)
 
-distinct_words = len(words_dict)
-print(distinct_words)
+print("\nOutput")
+print(len(words_dict))
+
 
 for key,value in words_dict.items():
     print(value, end = " ")
+
+key_list = list(words_dict.keys())
+val_list = list(words_dict.values())
+
+
+n1 = 1
+n2 = 1
+for value in val_list:
+    if n1 < value:
+        n1 = value
+    if n2 > value:
+        n2 = value
+
+position_highest = val_list.index(n1)
+position_lowest = val_list.index(n2)
+
+print("\n")
+print("\nThe most repeated word is '{}'.".format(key_list[position_highest]))
+print("The least repeated word is '{}'.".format(key_list[position_lowest]))
