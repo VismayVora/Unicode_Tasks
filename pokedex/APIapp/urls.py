4c36fd1ef1d23e9d67f063a54b19db342d7a29c6
Vismay Vora <voravismay9@gmail.com>
2021-08-08 00:20:52 +0530

    Minor update to previous commit

--- pokedex/APIapp/urls.py
+++ pokedex/APIapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 #from . import show_poke_form
   
 urlpatterns = [
-   path('', views.index, name='index'),
+   path('', views.index, name='index'),   
    path('types/', views.types, name='types'),
    #path('type/',show_poke_form.show_poke_form, name='show_poke_view'),
   ]
